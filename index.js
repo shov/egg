0b7559b57631f00c29bb9ebdcd9a8cab6019ac63
Alexandr Shevchenko <ls.shov@gmail.com>
2019-12-09 15:33:45 +0300

    Finished with parser, tested

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 'use strict'
 
+const codeExample=`
+  do(
+    define(x, 10),
+    if(>(x, 5),
+      print("large"),
+      print("small")
+     )
+  )
+`
+
+/**
+ * @type {Parser}
+ */
+const parser  = new (require('./lib/Parser'))()
+
+const result = parser.parse(codeExample)
+
+console.log(result)
